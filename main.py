d0178d50c320718f8262439a3cf3426adb5ebbe6
Anish Sebastian <anishst@hotmail.com>
2022-03-31 13:53:42 -0400

    added textbox and button

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import PyQt5.QtWidgets as qtw
+import PyQt5.QtGui as qtg
 
 class MainWindow(qtw.QWidget):
     def __init__(self):
@@ -11,8 +12,28 @@ class MainWindow(qtw.QWidget):
 
         # create a label
         my_label = qtw.QLabel("Hello world from PyQt5!")
+        #  change font and size
+        my_label.setFont(qtg.QFont('Arial', 18))
         self.layout().addWidget(my_label)
+
+        # create an entry box
+        my_entry = qtw.QLineEdit()
+        my_entry.setObjectName("name_field")
+        my_entry.setText("")
+        self.layout().addWidget(my_entry)
+
+        # create a button
+        my_button = qtw.QPushButton("Click Me!", clicked=lambda: press_it())
+        self.layout().addWidget(my_button)
+
+        # show the app
         self.show()
+
+        def press_it():
+            # add name to label
+            my_label.setText(f"Hello {my_entry.text()}!")
+            my_entry.setText("")
+
 
 app = qtw.QApplication([])
 mw = MainWindow()
